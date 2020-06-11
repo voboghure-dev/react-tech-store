44e57e7291aa4ff0d4bd7ec0cd1cad6fe690ac75
Tapan Kumer Das <tapan@devspalace.com>
2020-06-11 17:10:39 +0600

    Chapter 17
    Signed-off-by: Tapan Kumer Das <tapan@devspalace.com>

---
+++ src/components/Hero.js
@@ -0,0 +1,38 @@
+import React from "react";
+import styled from "styled-components";
+import mainBcg from "../images/mainBcg.jpeg";
+
+const Hero = (props) => {
+  return (
+    <HeroWrapper max={props.max} img={props.img}>
+      <div className="banner">
+        <h1 className="title">{props.title}</h1>
+        {props.children}
+      </div>
+    </HeroWrapper>
+  );
+};
+
+const HeroWrapper = styled.div`
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: ${(props) => (props.max ? "100vh" : "60vh")};
+  color: var(--mainWhite);
+  background: linear-gradient(var(--primaryRGBA), var(--primaryRGBA)),
+    url(${(props) => props.img}) center/cover no-repeat;
+  .title {
+    padding-top: 2rem;
+    font-size: 3.5rem;
+    text-shadow: 4px 4px 2px rgba(0, 0, 0, 0.3);
+    text-transform: uppercase;
+    letter-spacing: var(--mainSpacing);
+  }
+`;
+
+Hero.defaultProps = {
+  img: mainBcg,
+};
+
+export default Hero;
