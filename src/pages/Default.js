65f55248ac072dc5141003e100294386465afa39
Tapan Kumer Das <tapan@devspalace.com>
2020-06-11 17:28:12 +0600

    Chapter 18
    Signed-off-by: Tapan Kumer Das <tapan@devspalace.com>

--- src/pages/Default.js
+++ src/pages/Default.js
@@ -1,9 +1,17 @@
 import React from "react";
+import Hero from "../components/Hero";
+import { Link } from "react-router-dom";
+import defaultBcg from "../images/defaultBcg.jpeg";
 
 const Default = (props) => {
   return (
     <>
-      <h1>Hello from Default Page</h1>
+      <Hero img={defaultBcg} title="404" max="true">
+        <h2 className="text-uppercase">Page Not Found</h2>
+        <Link to="/" className="main-link" style={{ marginTop: "2rem" }}>
+          Back to Home
+        </Link>
+      </Hero>
     </>
   );
 };
