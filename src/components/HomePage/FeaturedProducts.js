2742d881512086afa363fb3c65f482fc4703dc5d
Tapan Kumer Das <tapan@devspalace.com>
2020-06-14 02:52:25 +0600

    Chapter 28
    Signed-off-by: Tapan Kumer Das <tapan@devspalace.com>

--- src/components/HomePage/FeaturedProducts.js
+++ src/components/HomePage/FeaturedProducts.js
@@ -1,7 +1,29 @@
 import React from "react";
+import Product from "../Product";
+import { Link } from "react-router-dom";
+import Title from "../Title";
+import { ProductConsumer } from "../../context";
 
 const FeaturedProducts = (props) => {
-  return <div>FeaturedProducts</div>;
+  return (
+    <section className="py-5">
+      <div className="container">
+        {/* title */}
+        <Title title="featured products" center="true" />
+        {/* products */}
+        <div className="row">
+          <ProductConsumer>
+            {(value) => {
+              const { featuredProducts } = value;
+              return featuredProducts.map((product) => (
+                <Product key={product.id} product={product} />
+              ));
+            }}
+          </ProductConsumer>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default FeaturedProducts;
