4e13f315b2fcafdb2c43785b6e64716b72c48cd3
Tapan Kumer Das <tapan@devspalace.com>
2020-06-15 03:00:23 +0600

    Chapter 36
    Signed-off-by: Tapan Kumer Das <tapan@devspalace.com>

--- src/components/SideCart.js
+++ src/components/SideCart.js
@@ -1,15 +1,43 @@
 import React from "react";
 import { ProductConsumer } from "../context";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 const SideCart = (props) => {
   return (
     <ProductConsumer>
       {(value) => {
-        const { cartOpen, closeCart, cart } = value;
+        const { cartOpen, closeCart, cart, cartTotal } = value;
         return (
           <CartWrapper show={cartOpen} onClick={closeCart}>
-            <p>Cart Items</p>
+            <ul>
+              {cart.map((item) => {
+                return (
+                  <li className="cart-item mb-4">
+                    <img
+                      width="35"
+                      src={`../${item.image}`}
+                      // src={item.image}
+                      alt="cart"
+                    />
+                    <div className="mt-3">
+                      <h6 className="text-uppercase">{item.title}</h6>
+                      <h6 className="text-title text-capitalize">
+                        quantity: {item.count}
+                      </h6>
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
+            <h4 className="text-capitalize text-main">
+              cart total: ${cartTotal}
+            </h4>
+            <div className="text-center my-5">
+              <Link to="/cart" className="main-link">
+                cart page
+              </Link>
+            </div>
           </CartWrapper>
         );
       }}
@@ -30,6 +58,14 @@ const CartWrapper = styled.div`
   transition: var(--mainTransition);
   @media (min-width: 576px) {
     width: 20rem;
+  }
+  overflow: scroll;
+  padding: 2rem;
+  ul {
+    padding: 0 !important;
+  }
+  .cart-item {
+    list-style-type: none;
   }
 `;
 
